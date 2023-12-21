25cbfb06e4772f59b4da0689a009e63eb7df1ad5
hazycora <hazysu@riseup.net>
2023-12-21 05:24:20 -0600

    add no-unused-vars rule
    for next version-bump

--- packages/create-besties/template/lib/.eslintrc.cjs
+++ packages/create-besties/template/lib/.eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
 		node: true
 	},
 	rules: {
-		'@typescript-eslint/no-explicit-any': 'warn'
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
 	}
 }
